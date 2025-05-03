3c0e1c294646cc7501228af058b5b8a6ea1b5a19
Kőszegi Patrik <koszegipatrik888@gmail.com>
2025-05-03 21:18:02 +0200

    Merged home assistant communicator code with main code.

--- smart-pot/smart-pot.ino
+++ smart-pot/smart-pot.ino
@@ -1,4 +1,16 @@
 #include <Adafruit_BME280.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi credentials
+const char* WIFI_SSID = "SSID_HERE";       // change
+const char* WIFI_PASSWORD = "PASSW_HERE";  // change
+
+// MQTT broker info
+const char* MQTT_SERVER_IP = "192.168.31.31";
+const int MQTT_SERVER_PORT = 1883;
+const char* MQTT_USERNAME = "okos-cserep";
+const char* MQTT_PASSWORD = "okoscserep123";
 
 // BME280 address
 const byte BME_ADDR = 0x76;
@@ -31,47 +43,75 @@ int waterLevel = 0;
 
 // Notification timing
 unsigned long lastWaterNotificationTime = 0;
-const unsigned long WATER_NOTIFICATION_INTERVAL = 5000; // change
+const unsigned long WATER_NOTIFICATION_INTERVAL = 5000;  // change
 bool waterNotifSent = false;
 bool firstWaterNotificationSent = false;
 
 // Watering control
 bool isWatering = false;
 unsigned long wateringStartTime = 0;
-const unsigned long WATERING_DURATION = 3000; // change
+const unsigned long WATERING_DURATION = 3000;  // change
 unsigned long lastWateringEndTime = 0;
-const unsigned long WATERING_COOLDOWN = 5000; // change
+const unsigned long WATERING_COOLDOWN = 5000;  // change
 bool hasWateredOnce = false;
 
-// BME280 instance
+// Instances
 Adafruit_BME280 bme;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
+// Function predefinitions
 void turnLedOn(char color = 'a');
+void setupWifi();
+void reconnect();
+void handleHighTemp();
+void handleDarkness();
+void handleOK();
+void handleLowWaterLevel();
+void waterPlant();
 
 void setup() {
-  Serial.begin(9600);
+  // Begin serial communication
+  Serial.begin(115200);
   Serial.println("Smart pot started!");
 
+  // Connect to WiFi
+  setupWifi();
+
+  // Connect to MQTT server
+  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+
+  // BME280 error handling
   if (!bme.begin(BME_ADDR)) {
     Serial.println("Couldn't find BME280!");
     while (1)
       ;
   }
 
+  // Input pins
   pinMode(LDR_PIN, INPUT);
   pinMode(MOISTURE_PIN, INPUT);
   pinMode(WATER_LEVEL_PIN, INPUT);
 
+  // Output pins
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
   pinMode(WATER_PUMP_PIN, OUTPUT);
 
+  // Default pin writings
   digitalWrite(WATER_PUMP_PIN, LOW);
   turnLedOn();  // All LEDs off
 }
 
 void loop() {
+  // Check if client is connected
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Default values
   unsigned long currentMillis = millis();
   char activeAlert = 'n';  // none
 
@@ -82,6 +122,10 @@ void loop() {
   moisture = analogRead(MOISTURE_PIN);
   waterLevel = analogRead(WATER_LEVEL_PIN);
 
+  // Buffer to store published data
+  char dataBuffer[10];
+
+  // Show real-time values in serial
   Serial.println("------------------------------");
   Serial.print("Temperature: ");
   Serial.println(temperature);
@@ -94,6 +138,25 @@ void loop() {
   Serial.print("Water Level: ");
   Serial.println(waterLevel);
   Serial.println("------------------------------");
+
+  // Send real-time values to home assistant
+  // Temperature
+  dtostrf(temperature, 1, 2, dataBuffer);
+  client.publish("okoscserep/temperature", dataBuffer);
+
+  // Humidity
+  dtostrf(humidity, 1, 2, dataBuffer);
+  client.publish("okoscserep/humidity", dataBuffer);
+
+  // Water level
+  sprintf(dataBuffer, "%d", waterLevel);
+  client.publish("okoscserep/water_level", dataBuffer);
+
+  // Soil moisture
+  sprintf(dataBuffer, "%d", moisture);
+  client.publish("okoscserep/soil_moisture", dataBuffer);
+
+  Serial.println("MQTT data sent!");
 
   // Stop watering after duration
   if (isWatering) {
@@ -155,8 +218,34 @@ void loop() {
   }
 }
 
+// Home assistant functions
+void setupWifi() {
+  Serial.println("Connecting to WiFi - ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connection established!");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT server...");
+    if (client.connect("ESP8266Client", MQTT_USERNAME, MQTT_PASSWORD)) {
+      Serial.println("MQTT server connection established!");
+    } else {
+      Serial.print("Error occured: ");
+      Serial.print(client.state());
+      Serial.println("Retrying in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
 // Utility Functions
-void turnLedOn(char color = 'a') {
+void turnLedOn(char color) {
   digitalWrite(RED_LED, HIGH);
   digitalWrite(GREEN_LED, HIGH);
   digitalWrite(BLUE_LED, HIGH);
